5d25336863ab61b19a3d40e0f6b47e6f7655f1a8
Scott W Harden <swharden@gmail.com>
2021-06-02 20:06:33 -0400

    IO: support saving 2D and 3D arrays

--- src/Cima.Test/IoTests.cs
+++ src/Cima.Test/IoTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace Cima.Test
 {
@@ -13,35 +14,43 @@ namespace Cima.Test
                     "../../../../../data/images/cat.png")
                 );
 
-        public static readonly string PATH_L =
+        public static readonly string PATH_GRAY =
             Path.GetFullPath(
                 Path.Combine(
                     TestContext.CurrentContext.TestDirectory,
                     "../../../../../data/images/coins.png")
                 );
 
+        private static string GetFileHash(string filePath)
+        {
+            var md5 = System.Security.Cryptography.MD5.Create();
+            using var stream = File.OpenRead(filePath);
+            byte[] bytes = md5.ComputeHash(stream);
+            return string.Join("", md5.ComputeHash(bytes).Select(x => x.ToString("x2")).ToArray()); ;
+        }
+
         [Test]
         public void Test_SampleImages_Exist()
         {
             Assert.That(File.Exists(PATH_RGB), PATH_RGB);
-            Assert.That(File.Exists(PATH_L), PATH_L);
+            Assert.That(File.Exists(PATH_GRAY), PATH_GRAY);
         }
 
         [Test]
         public void Test_Dimensions_RGB()
         {
             var bmp = IO.LoadBitmap(PATH_RGB);
-            var bytes3d = IO.BitmapToBytes3D(bmp);
+            var bytes3d = IO.LoadRGB(bmp);
             Assert.AreEqual(300, bytes3d.GetLength(0));
             Assert.AreEqual(451, bytes3d.GetLength(1));
             Assert.AreEqual(3, bytes3d.GetLength(2));
         }
 
         [Test]
-        public void Test_Dimensions_G()
+        public void Test_Dimensions_Gray()
         {
-            var bmp = IO.LoadBitmap(PATH_L);
-            var bytes2d = IO.BitmapToBytes2D(bmp);
+            var bmp = IO.LoadBitmap(PATH_GRAY);
+            var bytes2d = IO.LoadGrayscale(bmp);
             Assert.AreEqual(303, bytes2d.GetLength(0));
             Assert.AreEqual(384, bytes2d.GetLength(1));
         }
@@ -50,16 +59,16 @@ namespace Cima.Test
         public void Test_Flatten_RGB()
         {
             var bmp = IO.LoadBitmap(PATH_RGB);
-            var bytes3d = IO.BitmapToBytes3D(bmp);
+            var bytes3d = IO.LoadRGB(bmp);
             var bytes1d = Operations.Flatten(bytes3d);
             Assert.AreEqual(bmp.Width * bmp.Height * 3, bytes1d.Length);
         }
 
         [Test]
-        public void Test_Flatten_L()
+        public void Test_Flatten_Gray()
         {
-            var bmp = IO.LoadBitmap(PATH_L);
-            var bytes2d = IO.BitmapToBytes2D(bmp);
+            var bmp = IO.LoadBitmap(PATH_GRAY);
+            var bytes2d = IO.LoadGrayscale(bmp);
             var bytes1d = Operations.Flatten(bytes2d);
             Assert.AreEqual(bmp.Width * bmp.Height, bytes1d.Length);
         }
@@ -69,21 +78,87 @@ namespace Cima.Test
         {
             // compare hash to that calculated with Python/PIL
             var bmp = IO.LoadBitmap(PATH_RGB);
-            var bytes3d = IO.BitmapToBytes3D(bmp);
+            var bytes3d = IO.LoadRGB(bmp);
             var bytes1d = Operations.Flatten(bytes3d);
             string hash = Operations.MD5(bytes1d);
             Assert.AreEqual("4cbc8458da90b6c4b2dcf19e51656619", hash);
         }
 
         [Test]
-        public void Test_Hash_L()
+        public void Test_Hash_Gray()
         {
             // compare hash to that calculated with Python/PIL
-            var bmp = IO.LoadBitmap(PATH_L);
-            var bytes2d = IO.BitmapToBytes2D(bmp);
+            var bmp = IO.LoadBitmap(PATH_GRAY);
+            var bytes2d = IO.LoadGrayscale(bmp);
             var bytes1d = Operations.Flatten(bytes2d);
             string hash = Operations.MD5(bytes1d);
             Assert.AreEqual("651a9e413b9cc780d6ae9c5eca027c76", hash);
+        }
+
+        [Test]
+        public void Test_Save_RGB()
+        {
+            string saveFilePath = $"test_{Path.GetFileNameWithoutExtension(PATH_RGB)}.png";
+            var bmp1 = IO.LoadBitmap(PATH_RGB);
+            var bytes1 = IO.LoadRGB(bmp1);
+            var bmp2 = IO.GetBitmap(bytes1);
+            IO.SavePng(bmp2, saveFilePath);
+
+            var bmp = IO.LoadBitmap(saveFilePath);
+            var bytes2d = IO.LoadRGB(bmp);
+            var bytes1d = Operations.Flatten(bytes2d);
+            string hash = Operations.MD5(bytes1d);
+            Assert.AreEqual("4cbc8458da90b6c4b2dcf19e51656619", hash);
+        }
+
+        [Test]
+        public void Test_Save_Gray()
+        {
+            string saveFilePath = $"test_{Path.GetFileNameWithoutExtension(PATH_GRAY)}.png";
+            var bmp1 = IO.LoadBitmap(PATH_GRAY);
+            var bytes1 = IO.LoadGrayscale(bmp1);
+            var bmp2 = IO.GetBitmap(bytes1);
+            IO.SavePng(bmp2, saveFilePath);
+
+            var bmp = IO.LoadBitmap(saveFilePath);
+            var bytes2d = IO.LoadGrayscale(bmp);
+            var bytes1d = Operations.Flatten(bytes2d);
+            string hash = Operations.MD5(bytes1d);
+            Assert.AreEqual("651a9e413b9cc780d6ae9c5eca027c76", hash);
+        }
+
+        [TestCase("cat-119x79.png", 119, 79)]
+        [TestCase("cat-120x80.png", 120, 80)]
+        [TestCase("cat-121x80.png", 121, 80)]
+        [TestCase("cat-122x81.png", 122, 81)]
+        [TestCase("cat-123x82.png", 123, 82)]
+        [TestCase("cat-124x82.png", 124, 82)]
+        public void Test_SpanCalculation_IsAccurate(string filename, int width, int height)
+        {
+            // Even though bitmap images can be any width their bytes in memory always use widths that are multiples of 4.
+            // This means there will be empty bytes in memory for bitmaps with widths that aren't an even multiple of 4.
+            // A bad span (image width in memory) presents itself as an image that slants.
+            // These tests ensure span calculations are accurate by loading, saving, and comparing images of different widths.
+
+            string imageFolderPath = Path.Combine(TestContext.CurrentContext.TestDirectory, $"../../../../../data/images/");
+            string loadFilePath = Path.Combine(imageFolderPath, filename);
+
+            // load an input image of known size
+            var originalBitmap = IO.LoadBitmap(loadFilePath);
+            var originalBytes2d = IO.LoadGrayscale(originalBitmap);
+            Assert.AreEqual(originalBytes2d.GetLength(0), height);
+            Assert.AreEqual(originalBytes2d.GetLength(1), width);
+            string originalHash = Operations.MD5(originalBytes2d);
+
+            // save the image to a test file and re-load it
+            string saveFilePath = $"test_{filename}.png";
+            IO.SavePng(originalBitmap, saveFilePath);
+            var newBitmap = IO.LoadBitmap(saveFilePath);
+            var newBytes2d = IO.LoadGrayscale(newBitmap);
+            string newHash = Operations.MD5(newBytes2d);
+
+            // ensure the loaded data is identical
+            Assert.AreEqual(originalHash, newHash);
         }
     }
 }
--- src/Cima/IO.cs
+++ src/Cima/IO.cs
@@ -11,47 +11,47 @@ namespace Cima
     {
         public static Bitmap LoadBitmap(string filename) => new(filename);
 
-        public static byte[,] BitmapToBytes2D(Bitmap bmp)
+        public static byte[,] LoadGrayscale(Bitmap bmp)
         {
             // lock the image and copy all its bytes
             Rectangle rect = new(0, 0, bmp.Width, bmp.Height);
             BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
             int byteCount = Math.Abs(bmpData.Stride) * bmp.Height;
-            byte[] bytes = new byte[byteCount];
-            Marshal.Copy(bmpData.Scan0, bytes, 0, byteCount);
+            byte[] input = new byte[byteCount];
+            Marshal.Copy(bmpData.Scan0, input, 0, byteCount);
             bmp.UnlockBits(bmpData);
 
             // copy data from bytes into 2D array
             int bytesPerPixel = System.Drawing.Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
             int bytesPerRow = bmpData.Stride;
-            byte[,] data = new byte[bmp.Height, bmp.Width];
+            byte[,] output = new byte[bmp.Height, bmp.Width];
             for (int y = 0; y < bmp.Height; y++)
             {
                 int rowOffset = bytesPerRow * y;
                 for (int x = 0; x < bmp.Width; x++)
                 {
                     int pos = rowOffset + x * bytesPerPixel;
-                    data[y, x] = bytes[pos];
+                    output[y, x] = input[pos];
                 }
             }
 
-            return data;
+            return output;
         }
 
-        public static byte[,,] BitmapToBytes3D(Bitmap bmp)
+        public static byte[,,] LoadRGB(Bitmap bmp)
         {
             // lock the image and copy all its bytes
             Rectangle rect = new(0, 0, bmp.Width, bmp.Height);
             BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
             int byteCount = Math.Abs(bmpData.Stride) * bmp.Height;
-            byte[] bytes = new byte[byteCount];
-            Marshal.Copy(bmpData.Scan0, bytes, 0, byteCount);
+            byte[] input = new byte[byteCount];
+            Marshal.Copy(bmpData.Scan0, input, 0, byteCount);
             bmp.UnlockBits(bmpData);
 
             // copy data from bytes into 2D array
             int bytesPerPixel = System.Drawing.Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
             int bytesPerRow = bmpData.Stride;
-            byte[,,] data = new byte[bmp.Height, bmp.Width, bytesPerPixel];
+            byte[,,] output = new byte[bmp.Height, bmp.Width, bytesPerPixel];
             for (int y = 0; y < bmp.Height; y++)
             {
                 int rowOffset = bytesPerRow * y;
@@ -61,12 +61,74 @@ namespace Cima
                     {
                         // pixel bytes are little endian (RGB and ARGB)
                         int pos = rowOffset + x * bytesPerPixel + (bytesPerPixel - z - 1);
-                        data[y, x, z] = bytes[pos];
+                        output[y, x, z] = input[pos];
                     }
                 }
             }
 
-            return data;
+            return output;
         }
+
+        public static Bitmap GetBitmap(byte[,] input)
+        {
+            int height = input.GetLength(0);
+            int width = input.GetLength(1);
+
+            byte[] pixelsOutput = new byte[height * width];
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    pixelsOutput[y * width + x] = input[y, x];
+
+            Bitmap bmp = new(width, height, PixelFormat.Format8bppIndexed);
+            ApplyPalette_Grayscale(bmp);
+            var rect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format8bppIndexed);
+            Marshal.Copy(pixelsOutput, 0, bmpData.Scan0, pixelsOutput.Length);
+            bmp.UnlockBits(bmpData);
+
+            return bmp;
+        }
+
+        public static Bitmap GetBitmap(byte[,,] input)
+        {
+            int height = input.GetLength(0);
+            int width = input.GetLength(1);
+            int depth = input.GetLength(2);
+
+            PixelFormat bmpFormat = depth switch
+            {
+                3 => PixelFormat.Format24bppRgb,
+                4 => PixelFormat.Format32bppArgb,
+                _ => throw new NotImplementedException($"unsupported bytes per pixel ({depth})"),
+            };
+
+            int extraBytesPerRow = width % 4; // TODO: additional image width tests
+            int bytesPerRow = width * depth + extraBytesPerRow; // stride
+            byte[] pixelsOutput = new byte[height * bytesPerRow];
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    for (int z = 0; z < depth; z++)
+                        pixelsOutput[y * bytesPerRow + x * depth + (depth - z - 1)] = input[y, x, z];
+
+            Bitmap bmp = new(width, height, bmpFormat);
+            var rect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmpFormat);
+            Marshal.Copy(pixelsOutput, 0, bmpData.Scan0, pixelsOutput.Length);
+            bmp.UnlockBits(bmpData);
+
+            return bmp;
+        }
+
+        private static void ApplyPalette_Grayscale(Bitmap bmp)
+        {
+            ColorPalette pal = bmp.Palette;
+            for (int i = 0; i < 256; i++)
+                pal.Entries[i] = Color.FromArgb(255, i, i, i);
+            bmp.Palette = pal;
+        }
+
+        public static void SavePng(Bitmap bmp, string path) => bmp.Save(path, ImageFormat.Png);
     }
 }
