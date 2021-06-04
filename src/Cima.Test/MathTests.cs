78b86a88ee17d3f808155626dadf0092f00255e9
Scott W Harden <swharden@gmail.com>
2021-06-03 21:13:59 -0400

    create ImageMath module

---
+++ src/Cima.Test/MathTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima.Test
+{
+    public class MathTests
+    {
+        [Test]
+        public void Test_Math_Mean()
+        {
+            double[,] data = Sample.Data.Random_5x4;
+            double dataMean = ImageMath.Mean(data);
+            Assert.AreEqual(4.835, dataMean, delta: 1e-10);
+
+            double[] flat = Operations.Flatten(data);
+            double flatMean = ImageMath.Mean(flat);
+            Assert.AreEqual(flatMean, dataMean);
+        }
+    }
+}
---
+++ src/Cima/ImageMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima
+{
+    public class ImageMath
+    {
+        public static double Mean(double[,] vals)
+        {
+            int height = vals.GetLength(0);
+            int width = vals.GetLength(1);
+
+            double sum = 0;
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    sum += vals[i, j];
+
+            return sum / (height * width);
+        }
+
+        public static double Mean(double[] vals)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < vals.Length; i++)
+                sum += vals[i];
+
+            return sum / vals.Length;
+        }
+    }
+}
