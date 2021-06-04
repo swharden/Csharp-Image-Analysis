44a377dd36b3353e10755f0f9a8f0bdff65f069c
Scott W Harden <swharden@gmail.com>
2021-06-03 21:28:30 -0400

    create Display module

---
+++ src/Cima.Test/DisplayTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima.Test
+{
+    class DisplayTests
+    {
+        [Test]
+        public void Test_Display_IsNotNull()
+        {
+            double[,] data = Sample.Data.Random_5x4;
+            string disp = Display.Array(data);
+            Assert.IsNotNull(disp);
+            Console.WriteLine(disp);
+        }
+    }
+}
---
+++ src/Cima/Display.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima
+{
+    public static class Display
+    {
+        public static string Array(double[,] vals)
+        {
+            int width = vals.GetLength(1);
+            int height = vals.GetLength(0);
+            int count = width * height;
+
+            StringBuilder sb = new();
+            sb.Append($"2D array with {count} values (w={width}, h={height})");
+            for (int y = 0; y < height; y++)
+            {
+                sb.AppendLine();
+                for (int x = 0; x < width; x++)
+                {
+                    double val = vals[y, x];
+                    sb.Append($"{val, 7}");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
