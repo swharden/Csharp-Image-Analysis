bc33cf06b13ceea35fc5a52e8eaac8e66fe811d2
Scott W Harden <swharden@gmail.com>
2021-06-07 18:42:48 -0400

    Tests: refactor generation tests

---
+++ src/Cima.Test/Generate/Array/Noise.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima.Test.Generate.Array
+{
+    class Noise
+    {
+        [Test]
+        public void Test_Generate_Random()
+        {
+            Random rand = new(0);
+            double[,] data = Generate2D.Random(rand, 10, 6);
+            Console.WriteLine(Display.Array(data));
+            Assert.AreEqual(10, data.GetLength(1));
+            Assert.AreEqual(6, data.GetLength(0));
+
+            double mean1 = ImageMath.Mean(data);
+            Assert.AreEqual(.5675501235764863, mean1);
+
+            double mean2 = ImageMath.Mean(Operations.Flatten(data));
+            Assert.AreEqual(mean1, mean2);
+        }
+
+        [Test]
+        public void Test_Generate_RandomBig()
+        {
+            Random rand = new(0);
+            double[,] data = Generate2D.Random(rand, 600, 400);
+            Assert.AreEqual(600, data.GetLength(1));
+            Assert.AreEqual(400, data.GetLength(0));
+
+            double mean1 = ImageMath.Mean(data);
+            Assert.AreEqual(.5, mean1, .001);
+
+            double mean2 = ImageMath.Mean(Operations.Flatten(data));
+            Assert.AreEqual(mean1, mean2);
+
+            TestTools.SavePng(data);
+        }
+    }
+}
