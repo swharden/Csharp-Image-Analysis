e819b0acd5987daf660f7fd80a298e40df4346de
Scott W Harden <swharden@gmail.com>
2021-06-07 18:54:01 -0400

    Generate.Kernel: create Uniform()

---
+++ src/Cima.Test/Generate/Kernel/Uniform.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima.Test.Generate.Kernel
+{
+    class Uniform
+    {
+        [TestCase(0, 1)]
+        [TestCase(1, 3)]
+        [TestCase(2, 5)]
+        [TestCase(3, 7)]
+        public void Test_Shape_IsCorrect(int size, int width)
+        {
+            double[,] kernel = Cima.Generate.Kernel.Uniform(size, 123);
+            Assert.AreEqual(width, kernel.GetLength(0));
+            Assert.AreEqual(width, kernel.GetLength(1));
+        }
+
+        [TestCase(123)]
+        [TestCase(-321)]
+        public void Test_Values_MatchThoseGiven(int val)
+        {
+            double[,] kernel = Cima.Generate.Kernel.Uniform(3, val);
+            foreach (double v in Operations.Flatten(kernel))
+                Assert.AreEqual(val, v);
+        }
+
+        public void Test_Size_CannotBeNegative(int val)
+        {
+            Assert.Throws<ArgumentException>(() => Cima.Generate.Kernel.Uniform(3, val));
+        }
+    }
+}
---
+++ src/Cima/Generate/Kernel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cima.Generate
+{
+    public static class Kernel
+    {
+        /// <summary>
+        /// Return a square kernel with all values the same
+        /// </summary>
+        /// <param name="value">set every element in the array to this value</param>
+        /// <param name="size">number of pixels between the center pixel and each edge</param>
+        /// <returns></returns>
+        public static double[,] Uniform(int size, double value)
+        {
+            if (size < 0)
+                throw new ArgumentException("size must be positive");
+
+            int width = size * 2 + 1;
+            double[,] kernel = new double[width, width];
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < width; j++)
+                    kernel[i, j] = value;
+            return kernel;
+        }
+
+        public static double[,] Gaussian(int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
