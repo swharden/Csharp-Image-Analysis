807be4d5553540a964a1f6a5a151489571b70933
Scott W Harden <swharden@gmail.com>
2021-06-07 20:38:55 -0400

    Statistics: Sum (2D)

--- src/Cima/Statistics.cs
+++ src/Cima/Statistics.cs
@@ -6,6 +6,19 @@ namespace Cima
 {
     public class Statistics
     {
+        public static double Sum(double[,] vals)
+        {
+            int height = vals.GetLength(0);
+            int width = vals.GetLength(1);
+
+            double sum = 0;
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    sum += vals[i, j];
+
+            return sum;
+        }
+
         public static double Mean(double[,] vals)
         {
             int height = vals.GetLength(0);
